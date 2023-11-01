f2fdf54ac2191f2fd353f5c9510ef914d8bff28d
4LPH4-V1P3R <hkyle525@gmail.com>
2023-10-31 23:33:16 -0400

    Separated Keydown/Keyup events and added q to quit.

--- alien_invasion.py
+++ alien_invasion.py
@@ -29,17 +29,29 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+
                 elif event.type == pygame.KEYDOWN:
+                     self._check_keydown_events(event)
+                elif event.type == pygame.KEYUP:
+                     self._check_keyup_events(event)
+
+
+    def _check_keydown_events(self, event):
+                     """Respond to keypresses."""
                      if event.key == pygame.K_RIGHT:
                         self.ship.moving_right = True
                      elif event.key == pygame.K_LEFT:
                         self.ship.moving_left = True
-                elif event.type == pygame.KEYUP:
+                     elif event.key == pygame.K_q:
+                           sys.exit()
+
+    def _check_keyup_events(self, event):
+                     """Respond to key releases."""
                      if event.key == pygame.K_RIGHT:
                         self.ship.moving_right = False
                      elif event.key == pygame.K_LEFT:
                         self.ship.moving_left = False
-                        # Move the ship to the right.
+                        
                         self.ship.rect.x += 1
 
             # Redraw the screen during each pass through the loop.
