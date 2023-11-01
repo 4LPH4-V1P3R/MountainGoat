40ef22b9746693500377cfc85bf9c3cd68b1d2e3
4LPH4-V1P3R <hkyle525@gmail.com>
2023-10-31 22:44:27 -0400

    Moving left and right with adjusted ship speed.

--- alien_invasion.py
+++ alien_invasion.py
@@ -23,12 +23,31 @@ class AlienInvasion:
     def run_game(self):
         """start the main loop for the game."""
         while True:
-            # Watch for keyboard and mouse events.
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
+        
+    def _check_events(self):
+            """Respond to Keypresses and mouse events."""
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+                elif event.type == pygame.KEYDOWN:
+                     if event.key == pygame.K_RIGHT:
+                        self.ship.moving_right = True
+                     elif event.key == pygame.K_LEFT:
+                        self.ship.moving_left = True
+                elif event.type == pygame.KEYUP:
+                     if event.key == pygame.K_RIGHT:
+                        self.ship.moving_right = False
+                     elif event.key == pygame.K_LEFT:
+                        self.ship.moving_left = False
+                        # Move the ship to the right.
+                        self.ship.rect.x += 1
 
             # Redraw the screen during each pass through the loop.
+    def _update_screen(self):
+            """Update images on the screen, and flip to the new screen."""
             self.screen.fill(self.settings.bg_color)
             self.ship.blitme()
 
--- settings.py
+++ settings.py
@@ -5,4 +5,7 @@ class Settings:
         # Screen Settings
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (230, 230, 230)
+        self.bg_color = (252, 252, 252)
+
+        # Ship Settings
+        self.ship_speed = 1.5
