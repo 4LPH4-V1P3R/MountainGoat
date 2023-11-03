98cf95f25c4075c68f693b9a3d5a92936655d4ab
4LPH4-V1P3R <hkyle525@gmail.com>
2023-11-03 15:14:37 -0400

    Created first row of aliens.

--- alien_invasion.py
+++ alien_invasion.py
@@ -100,9 +100,20 @@ class AlienInvasion:
 
     def _create_fleet(self):
         """Create the fleet of aliens."""
-        # Make an alien.
+        # Create an alien and find the number of aliens in a row.
+        # Spacing between each alien is equal to one alien width.
         alien = Alien(self)
-        self.aliens.add(alien)
+        alien_width = alien.rect.width
+        available_space_x = self.settings.screen_width - (2 * alien_width)
+        number_aliens_x = available_space_x // (2 * alien_width)
+
+        # Create the first row of aliens.
+        for alien_number in range(number_aliens_x):
+             # Create an alien and place it in a the row.
+             alien = Alien(self)
+             alien.x = alien_width + 2 * alien_width * alien_number 
+             alien.rect.x = alien.x
+             self.aliens.add(alien)
 
 if __name__ == '__main__':
     # Make a game instance, and run the game.
