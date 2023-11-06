bd9f4ac9ed0fb7e6f575f1c0f241540d63dd45d9
4LPH4-V1P3R <hkyle525@gmail.com>
2023-11-05 20:52:14 -0500

    Game freezes when all ships are used.

--- game_stats.py
+++ game_stats.py
@@ -8,3 +8,6 @@ class GameStats:
     def reset_stats(self):
         """Initialize statistics that can change during the game."""
         self.ships_left = self.settings.ship_limit
+
+        # Start Alien Invasion in an active state.
+        self.game_active = True
