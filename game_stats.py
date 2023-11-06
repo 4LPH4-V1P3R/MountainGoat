c45c09ddbca03362f9a3dcca5649e9a8c2071956
4LPH4-V1P3R <hkyle525@gmail.com>
2023-11-05 22:46:31 -0500

    High score added

--- game_stats.py
+++ game_stats.py
@@ -5,6 +5,9 @@ class GameStats:
         self.settings = ai_game.settings
         self.reset_stats()
 
+        # High score should neve re reset.
+        self.high_score = 0
+
     def reset_stats(self):
         """Initialize statistics that can change during the game."""
         self.ships_left = self.settings.ship_limit
--- settings.py
+++ settings.py
@@ -22,6 +22,9 @@ class Settings:
         # How quickly the game speeds up.
         self.speedup_scale = 1.1
 
+        # How quickly the alien point values increase.
+        self.score_scale = 1.5
+
         self.initialize_dynamic_settings()
 
     def initialize_dynamic_settings(self):
@@ -37,7 +40,10 @@ class Settings:
         self.alien_points = 50
 
     def increase_speed(self):
-        """Increase speed settings."""
+        """Increase speed settings and alien point values."""
         self.ship_speed *= self.speedup_scale
         self.bullet_speed *= self.speedup_scale
         self.alien_speed *= self.speedup_scale
+
+        self.alien_points = int(self.alien_points * self.score_scale)
+        
